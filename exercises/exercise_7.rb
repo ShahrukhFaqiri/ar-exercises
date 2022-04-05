71797358f7d1ffb669b112c0e824f365da78ed12
Sha Faqiri <shahrukh.faqiri@gmail.com>
2022-04-05 19:52:24 +0000

    add ex 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,21 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+class Employee < ActiveRecord::Base
+  belongs_to :store
+  validates :first_name, :last_name, :hourly_rate,:store,
+end
+
+class Store < ActiveRecord::Base
+  has_many :employees
+  validates :name, :annual_revenue, :mens_apparel, :womens_apparel, presence: true 
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0}  
+  validates :name, length: { minimum: 3 }
+  validate :carry_apparel   
+ end
+
+puts "Store: "
+store_name = gets.chomp.to_s
+
+new_store = Store.create(name: store_name, annual_revenue: 1200000, mens_apparel: true, womens_apparel: true)
